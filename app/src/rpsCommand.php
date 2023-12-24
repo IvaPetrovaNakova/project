c811b52520e82a76a936e6007781f46e0ef5ef39
iva <georgieva_iva@abv.bg>
2023-12-24 12:10:52 +0200

    t

--- app/src/rpsCommand.php
+++ app/src/rpsCommand.php
@@ -1,7 +1,5 @@
 <?php
 
-    namespace App;
-
     use Symfony\Component\Console\Command\Command;
     use Symfony\Component\Console\Input\InputInterface;
     use Symfony\Component\Console\Output\OutputInterface;
